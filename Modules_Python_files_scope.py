f5742ce1858f4f99a18de2e0fa188d55df57a641
Aidan Baird <102356055+AidanBaird@users.noreply.github.com>
2022-04-10 20:52:17 +0100

    Add files via upload

---
+++ Modules_Python_files_scope.py
@@ -0,0 +1,7 @@
+from library import always_three
+
+always_three()
+
+
+def always_three():
+    return 3
---
+++ Modules_python_random.py
@@ -0,0 +1,17 @@
+# Import random below:
+
+import random
+
+# Create random_list below:
+
+random_list = [random.randint(1,100) for i in range(101)]
+
+# Create randomer_number below:
+
+randomer_number = random.choice(random_list)
+
+# Print randomer_number below:
+
+print(random_list)
+print("")
+print(randomer_number)
